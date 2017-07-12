8d50efca9446f701e47d0200cc2d46e879d64dae
Healforce <healforce@BindeMac-mini.local>
2017-07-12 11:09:43 +0800

    a

---
+++ CustomCollectionView/CWCollectionView.m
@@ -0,0 +1,35 @@
+//
+//  CWCollectionView.m
+//  CWCollectionFlow
+//
+//  Created by Healforce on 2017/7/7.
+//  Copyright © 2017年 ChrisWei. All rights reserved.
+//
+
+#import "CWCollectionView.h"
+
+@implementation CWCollectionView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+- (instancetype)init
+{
+    self = [super init];
+    if (self) {
+        
+    }
+    return self;
+}
+
+- (void)loadSubView
+{
+    
+}
+
+@end
